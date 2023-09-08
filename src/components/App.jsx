56bd012f2f6098b8c12cd99e8e36122bdea6b0b5
Mariya B <supermariyabros@gmail.com>
2023-09-08 18:56:05 +0300

    Add Banner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './NavBar'
+import Banner from './Banner'
 
 function App() {
   return (
     <div>
        <NavBar />
+       <Banner />
     </div>
   )
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="Banner-BG">
+      <section className="Banner">
+        <div className="context">
+          <h3> Karak Chai </h3>
+          <p>
+            Karak chai offers a diverse selection of teas sourced from various
+            regions for unique flavors and aromas.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Banner;
